f6bb2387394e33f27b07fb7183879a467713e720
Shivang Verma <shivangverma775@gmail.com>
2026-07-09 17:30:37 +0530

    Add The code

---
+++ 12-Advance Python/2-Types.py
@@ -0,0 +1,10 @@
+from typing import List, Tuple, Dict, Set
+n: List[int] = [1, 2, 3, 4, 5]
+t: Tuple[int, str, float] = (1, "Hello", 3.14)
+d: Dict[str, int] = {"one": 1, "two": 2, "three": 3}
+s: Set[str] = {"apple", "banana", "cherry"}
+
+print("List:", n)
+print("Tuple:", t)
+print("Dictionary:", d)
+print("Set:", s)
